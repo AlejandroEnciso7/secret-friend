f83be5f2ea90c34175f4da2e7b42eff17eb8ba7d
AlejandroEnciso7 <133036654+AlejandroEnciso7@users.noreply.github.com>
2025-03-11 15:01:50 -0700

    upload final version
    final version with add name, validate entry, shows list, sort list and clear name functions.

---
+++ app.js
@@ -0,0 +1,69 @@
+let amigos = [];
+
+/**
+ * Agrega un nuevo amigo al array de amigos.
+ * Valida que el nombre no esté vacío antes de agregarlo y actualiza la lista visual.
+ */
+function agregarAmigo() {
+  let nombreAmigo = document.getElementById("amigo").value;
+
+  if (nombreAmigo.trim() === "") {
+    alert("Por favor, inserte un nombre");
+  } else {
+    amigos.push(nombreAmigo);
+    document.querySelector("#amigo").value = "";
+    mostrarListaAmigo();
+  }
+}
+
+/**
+ * Actualiza la visualización de la lista de amigos en el DOM, creando elementos <li> para cada amigo.
+ * Cada elemento incluye un botón para eliminarlo de la lista.
+ */
+function mostrarListaAmigo() {
+  let listaAmigos = document.querySelector("#listaAmigos");
+  listaAmigos.innerHTML = "";
+
+  for (let index = 0; index < amigos.length; index++) {
+    const element = amigos[index];
+
+    let listaHTML = document.createElement("li");
+    listaHTML.textContent = element;
+
+    let deleteButton = document.createElement("button");
+    deleteButton.textContent = "Eliminar";
+    deleteButton.onclick = function() {
+      amigos.splice(index, 1);
+      mostrarListaAmigo();
+    };
+
+    listaHTML.appendChild(deleteButton);
+    listaAmigos.appendChild(listaHTML);
+  }
+
+  // Mostrar u ocultar el botón de limpiar lista
+  document.getElementById("clearButton").style.display = amigos.length > 0 ? "inline-block" : "none";
+}
+
+/**
+ * Sortea y muestra un amigo de la lista de amigos de manera aleatoria.
+ * Verifica que la lista no esté vacía antes de realizar el sorteo.
+ */
+function sortearAmigo() {
+  let cantidadAmigos = amigos.length;
+  if (cantidadAmigos === 0) {
+    alert("Por favor, inserte un nombre antes de sortear");
+  } else {
+    let indiceAmigo = Math.floor(Math.random() * cantidadAmigos);
+    let resultadoHTML = document.querySelector("#resultado");
+    resultadoHTML.innerHTML = amigos[indiceAmigo];
+  }
+}
+
+/**
+ * Limpia la lista de amigos y actualiza la visualización.
+ */
+function limpiarLista() {
+  amigos = [];
+  mostrarListaAmigo();
+}
